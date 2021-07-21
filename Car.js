247cd6069b75a0390909dea970d88368e035b46d
Atru8729 <a.truskauskas1@gmail.com>
2021-07-21 19:07:43 +0300

    doors working

--- Car.js
+++ Car.js
@@ -6,6 +6,26 @@ class Car {
     intro() {
         console.log(`This is ${this.data.brand}.`);
     }
+    doorsCount() {
+
+        console.log(`${this.data.brand} has ${this.data.doors.length} doors.`);
+    }
+    doorsWorking() {
+        let workingDoorsCount = 0;
+        for (let i = 0; i < this.data.doors.length; i++) {
+            const doors = this.data.doors[i];
+            if (doors.working === true) {
+                ++workingDoorsCount;
+            }
+        }
+        if (this.data.doors.length === workingDoorsCount) {
+
+            console.log(`${this.data.brand} has ${this.data.doors.length} doors and all works!`);
+        } else {
+
+            console.log(`${this.data.brand} has ${this.data.doors.length} doors, but only ${workingDoorsCount} is working.`);
+        }
+    }
 }
 
 module.exports = Car;
